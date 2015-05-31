0d53a6b7aa98efdc44133cec66e948532f978a10
Lukasz Mentel <lmmentel@gmail.com>
2015-05-31 02:33:33 +0200

    corrected some of the docstring in zefram

--- zefram/zefram.py
+++ zefram/zefram.py
@@ -11,8 +11,6 @@ from sqlalchemy.ext.declarative import declarative_base
 #from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.hybrid import hybrid_property
 
-__all__ = ['framework']
-
 def framework(codes):
 
     if isinstance(codes, (list, tuple)):
@@ -57,8 +55,7 @@ class RingSize(Base):
     Framework ring sizes
 
     Attributes
-    ----------
-    size : int
+      size : int
         Number of T atoms forming the ring
     '''
 
@@ -75,8 +72,7 @@ class NaturalTiling(Base):
     Framework natural tiling
 
     Attributes
-    ----------
-    name : str
+      name : str
         Name of the tiling, http://izasc.fos.su.se/fmi/xsl/IZA-SC/TilingList.htm
     '''
 
@@ -92,17 +88,16 @@ class SpaceGroup(Base):
     '''
     Space Group
 
-    Attributes
-    ----------
-    name : str
+    Attributes:
+      name : str
         Name of the space group
-    number : int
+      number : int
         Space group integer label according to International Tables for
         Crystallography Volume A: Space-group symmetry,
         URL:http://it.iucr.org/A/
-    cell_setting : str
+      cell_setting : str
         Additional cell settings
-    cs_code : str
+      cs_code : str
         Coordinate system code
     '''
 
@@ -122,28 +117,27 @@ class TAtom(Base):
     '''
     Symmetry unique T-atom
 
-    Attributes
-    ----------
-    framework_id : int
+    Attributes:
+      framework_id : int
         id number of the framework
-    name : str
+      name : str
         Name of the T-atom
-    x : float
+      x : float
         x coordinate
-    y : float
+      y : float
         y coordiante
-    z : float
+      z : float
         z coordiante
-    csq : str
+      csq : str
         A string representing the coordination sequence with values separated
         by a pipe '|'
-    multiplicity : int
+      multiplicity : int
         Site multiplicity
-    sym_restrictions : str
+      sym_restrictions : str
         Symmetry restrictions
-    site_symmetry : int
+      site_symmetry : int
         Site symmetry
-    vertex_symbol : str
+      vertex_symbol : str
         Vertex symbol
     '''
 
@@ -179,117 +173,114 @@ class Framework(Base):
     '''
     Zeolite framework.
 
-    Attributes
-    ----------
-    code : str
+    Attributes:
+      code : str
         Three letter framework code
-    name : str
+      name : str
         Name of the framework
-    atoms : int
+      atoms : int
         Number of atoms in the unit cell
-    portals : int
+      portals : int
         Number of portals in the unit cell
-    channels : int
+      channels : int
         Number of channels in the unit cell
-    cages : int
+      cages : int
         Number of cages in the unit cell
-    junctions : int
+      junctions : int
         Number of junctions in the unit cell
-    connections : int
+      connections : int
         Number of atom connections in the unit cell
-    tpv_abs : float
+      tpv_abs : float
         Total pore volume in cm^3/g
-    tpv_rel : float
+      tpv_rel : float
         Relative total pore volume,  % of the total volume
-    lcd : float
+      lcd : float
         Largest cavity diameter in Angstrom
-    pld : float
+      pld : float
         Pore limiting diameter in Angstrom
-    a : float
-        Unit cell length 'a' in Angstrom
-    b : float
-        Unit cell length 'b' in Angstrom
-    c : float
-        Unit cell length 'c' in Angstrom
-    alpha : float
-        Unit cell angle 'alpha' in degrees
-    beta : float
-        Unit cell angle 'beta' in degrees
-    gamma : float
-        Unit cell angle 'gamma' in degrees
-        Space group integer label according to International Tables for Crystallography
-        Volume A: Space-group symmetry, URL: http://it.iucr.org/A/
-    accessible_area : float
+      a : float
+        Unit cell length *a* in Angstrom
+      b : float
+        Unit cell length *b* in Angstrom
+      c : float
+        Unit cell length *c* in Angstrom
+      alpha : float
+        Unit cell angle *alpha* in degrees
+      beta : float
+        Unit cell angle *beta* in degrees
+      gamma : float
+        Unit cell angle *gamma* in degrees
+      accessible_area : float
         The area of that surface visited by the center of a (spherical) water
         molecule (radius = 1.4 Angstrom) in Angstrom squared [Angstrom^2].
-    accessible_area_m2pg : float
+      accessible_area_m2pg : float
         The area of that surface visited by the center of a (spherical) water
         molecule (radius = 1.4 Angstrom) in squared meters per gram [m^2/g].
-    accessible_volume : float
+      accessible_volume : float
         That portion of the occupiable volume that has continuity between all
         unit unit cells. Some pores/cavities have windows that are too small to
         allow the water molecule access, and so represent isolated occupiable
         regions [Angstrom^3].
-    accessible_volume_pct : float
+      accessible_volume_pct : float
         That portion of the occupiable volume that has continuity between all
         unit unit cells. Some pores/cavities have windows that are too small to
         allow the water molecule access, and so represent isolated occupiable
         regions [%].
-    channel_dim : int
+      channel_dim : int
         Number of dimension of the channel system
-    cif : str
+      cif : str
         Contents of the CIF file
-    framework_density : float
+      framework_density : float
         Frameork density, the number of T-atoms per 1000 Angstrom^3
-    isinterrupted : bool
+      isinterrupted : bool
         Flag indicating if the framework type is interrupted
-    isdisordered : bool
+      isdisordered : bool
         Flag indicating if the framework type is disordered
-    maxdsd_a : float
+      maxdsd_a : float
         Maximum diameter of a sphere that can diffuse along 'a'
-    maxdsd_b : float
+      maxdsd_b : float
         Maximum diameter of a sphere that can diffuse along 'b'
-    maxdsd_c : float
+      maxdsd_c : float
         Maximum diameter of a sphere that can diffuse along 'c'
-    maxdsi : float
+      maxdsi : float
         Maximum diameter of a sphere that can be inlcuded
-    occupiable_area : float
+      occupiable_area : float
         The area of that surface visited by the center of a (sperical) water
         molecule (radius = 1.4 Angstrom) [Angstrom^2]
-    occupiable_area_m2pg : float
+      occupiable_area_m2pg : float
         The area of that surface visited by the center of a (sperical) water
         molecule (radius = 1.4 Angstrom) [m^2/g]
-    occupiable_volume = float
+      occupiable_volume = float
         That portion of the available volume within the cell that can be
         visited by the center of a (spherical) water molecule (radius = 1.4
         Angstrom). The available volume is the unit cell volume remaining after
         the van der Waals atomic sphere volumes are subtracted [Angstrom^3].
-    occupiable_volume_pct : float
+      occupiable_volume_pct : float
         That portion of the available volume within the cell that can be
         visited by the center of a (spherical) water molecule (radius = 1.4
         Angstrom). The available volume is the unit cell volume remaining after
         the van der Waals atomic sphere volumes are subtracted [%].
-    rdls : float
+      rdls : float
         R_DLS
-    sbu : str
+      sbu : str
         Secondary building units
-    specific_accessible_area : float
+      specific_accessible_area : float
         Accessible area per unit volume
-    specific_occupiable_area : float
+      specific_occupiable_area : float
         Occupiable area per unit volume
-    td10 : float
+      td10 : float
         Approximation to the topological density terminated at the 10th item of
         the coordination sequence, see "Atlas of Zeolite Framework Types",
         Ch. Baerlocher, W. M. Meier, D. H. Olson, Fifth Revised Edition, Elsevier
         2001, pp. 17.
-    topological_density : float
+      topological_density : float
         Topological density, see "Atlas of Zeolite Framework Types",
         Ch. Baerlocher, W. M. Meier, D. H. Olson, Fifth Revised Edition, Elsevier
         2001, pp. 17.
-    url_iza : str
+      url_iza : str
         URL on the IZA website (http://izasc.fos.su.se/fmi/xsl/IZA-SC/ft.xsl)
         from which the relevant data was extracted
-    url_zeomics : str
+      url_zeomics : str
         URL on the ZEOMICS wbesite (http://helios.princeton.edu/zeomics/) from
         which the data was extracted
     '''
